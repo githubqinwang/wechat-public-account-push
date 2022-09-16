6dacdbe206754048f1855f5fde97ab3df819d99d
githubqinwang <113618501+githubqinwang@users.noreply.github.com>
2022-09-16 12:42:33 +0800

    test7

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oqeBB6Yart1GGeJHYQBuK8L9XEQQ",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "UQSdLjZckXZ7Ki-cHVMWcY96QgCzMGuaDNbjdUjRW0s",
+      useTemplateId: "WDqZObihwhfxyk3aDEPPiV-kdUJMwTP1g0ATE8J1QTM",
       // 所在省份
       province: "贵州",
       // 所在城市
